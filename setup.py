614c7f73c4e717e165f6602dc8fcfe7e414a0835
keiko <keikonagami3@gmail.com>
2025-07-30 11:39:58 -0700

    changing packge name

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="vista",
+    name="vista_map",
     version="0.1.0",
     author="Timothy Chen",
     author_email="chengine@stanford.edu",
